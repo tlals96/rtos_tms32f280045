84c4845a6bfe109e011dfb83d9d3aa4e4bd20475
tlals96 <tlals96@gmail.com>
2021-09-28 16:57:18 +0900

    project create
    first commit

---
+++ rtos_sys-bios/device/driverlib.h
@@ -0,0 +1,85 @@
+//#############################################################################
+//
+// FILE:   driverlib.h
+//
+// TITLE:  C28x Driverlib Header File
+//
+//#############################################################################
+// $TI Release: F28004x Support Library v1.12.00.00 $
+// $Release Date: Fri Feb 12 18:57:27 IST 2021 $
+// $Copyright:
+// Copyright (C) 2021 Texas Instruments Incorporated - http://www.ti.com/
+//
+// Redistribution and use in source and binary forms, with or without 
+// modification, are permitted provided that the following conditions 
+// are met:
+// 
+//   Redistributions of source code must retain the above copyright 
+//   notice, this list of conditions and the following disclaimer.
+// 
+//   Redistributions in binary form must reproduce the above copyright
+//   notice, this list of conditions and the following disclaimer in the 
+//   documentation and/or other materials provided with the   
+//   distribution.
+// 
+//   Neither the name of Texas Instruments Incorporated nor the names of
+//   its contributors may be used to endorse or promote products derived
+//   from this software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// $
+//#############################################################################
+#ifndef DRIVERLIB_H
+#define DRIVERLIB_H
+
+#include "driverlib/inc/hw_memmap.h"
+
+#include "driverlib/adc.h"
+#include "driverlib/asysctl.h"
+#include "driverlib/can.h"
+#include "driverlib/cla.h"
+#include "driverlib/clb.h"
+#include "driverlib/clapromcrc.h"
+#include "driverlib/cmpss.h"
+#include "driverlib/cpu.h"
+#include "driverlib/cputimer.h"
+#include "driverlib/dac.h"
+#include "driverlib/dcc.h"
+#include "driverlib/dcsm.h"
+#include "driverlib/debug.h"
+#include "driverlib/dma.h"
+#include "driverlib/ecap.h"
+#include "driverlib/epwm.h"
+#include "driverlib/eqep.h"
+#include "driverlib/erad.h"
+#include "driverlib/flash.h"
+#include "driverlib/fsi.h"
+#include "driverlib/gpio.h"
+#include "driverlib/hrcap.h"
+#include "driverlib/hrpwm.h"
+#include "driverlib/i2c.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/lin.h"
+#include "driverlib/memcfg.h"
+#include "driverlib/pga.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/pin_map_legacy.h"
+#include "driverlib/pmbus.h"
+#include "driverlib/sci.h"
+#include "driverlib/sdfm.h"
+#include "driverlib/spi.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/version.h"
+#include "driverlib/xbar.h"
+
+#endif  // end of DRIVERLIB_H definition
